ca2abbba0170638453a4990b5d2e8b5f742968ff
Stéphane Denis <sdenis@cegepsth.qc.ca>
2017-04-20 23:44:20 -0400

    nouvelle librairie

---
+++ AudioControlPT2257.cpp
@@ -0,0 +1,92 @@
+﻿#include <Arduino.h>
+#include <Wire.h>
+#include "AudioControlPT2257.h"
+
+AudioControlPT2257::AudioControlPT2257(){
+	writeMute(false);
+	writeVolume(0);
+}
+
+void AudioControlPT2257::increase() {
+  volume++;
+  if (volume > 79) volume = 79;
+
+  writeVolume(volume);
+
+  if (mute) {
+    mute = false;
+    writeMute(mute);
+  }
+}
+
+void AudioControlPT2257::decrease() {
+  volume--;
+  if (volume < 0)volume = 0;
+
+  writeVolume(volume);
+
+  if (mute) {
+    mute = false;
+    writeMute(mute);
+  }
+}
+
+void AudioControlPT2257::toggleMute() {
+  if (mute) mute = false;
+  else mute = true;
+  writeMute(mute);
+}
+
+void AudioControlPT2257::serialPrintStateJSON() {
+  Serial.print("{");
+  Serial.print("volume:");
+  Serial.print(volume);
+  Serial.print("; mute:");
+  Serial.print(mute);
+  Serial.println("}");
+}
+
+byte AudioControlPT2257::level(uint8_t dB) {
+
+  if (dB > 79) dB = 79;
+
+  int b = dB / 10; //get the second digit
+  int a = dB % 10; //get the first digit
+
+  b = b & 0b0000111; //limit the most significant digit to 3 bit (7)
+
+  return (b << 4) | a; //return both numbers in one byte (0BBBAAAA)
+}
+
+
+void AudioControlPT2257::writeVolume(uint8_t dB) {
+  byte bbbaaaa = level(dB);
+
+  byte aaaa = bbbaaaa & 0b00001111;
+  byte bbb = (bbbaaaa >> 4) & 0b00001111;
+
+  Wire.beginTransmission(PT2257_ADDR);
+  Wire.write(EVC_2CH_10 | bbb);
+  Wire.write(EVC_2CH_1 | aaaa);
+  Wire.endTransmission();
+}
+
+void AudioControlPT2257::writeMute(bool state) {
+  Wire.beginTransmission(PT2257_ADDR);
+  Wire.write(EVC_MUTE | (state & 0b00000001));
+  Wire.endTransmission();
+}
+
+void AudioControlPT2257::off() {
+  Wire.beginTransmission(PT2257_ADDR);
+  Wire.write(EVC_OFF);
+  Wire.endTransmission();
+}
+
+uint8_t AudioControlPT2257::getVolume(){
+  return volume;
+}
+
+bool AudioControlPT2257::getMute(){
+  return mute;
+}
---
+++ AudioControlPT2257.h
@@ -0,0 +1,81 @@
+﻿#ifndef AudioControlPT2257_h
+#define AudioControlPT2257_h
+#include <Arduino.h>
+
+/*
+  PT2257 - Electronic Volume Controller IC
+  Datasheet - http://www.princeton.com.tw/Portals/0/Product/PT2257.pdf
+
+  Pinout
+     |-----_-----|
+  1 -| Lin   Rin |- 8
+  2 -| Lout Rout |- 7
+  3 -| Gnd    V+ |- 6
+  4 -| SDA   SCL |- 5
+     |-----------|
+
+  The interface protocol consists of the following:
+  • A Start bit
+  • A Chip Address byte=88H 10001000
+  • ACK=Acknowledge bit
+  • A Data byte
+  • A Stop bit
+
+  Max. clock speed=100K bits/s
+
+  FUNCTION BITS
+  MSB    2    3    4    5    6    7    LSB  Function
+  ----------------------------------------------------------------------------------------
+  1    1    1    1    1    1    1    1    Function OFF (-79dB)
+  1    1    0    1    A3   A2   A1   A0   2-Channel, -1dB/step
+  1    1    1    0    0    B2   B1   B0   2-Channel, -10dB/step
+  1    0    1    0    A3   A2   A1   A0   Left Channel, -1dB/step
+  1    0    1    1    0    B2   B1   B0   Left Channel, -10dB/step
+  0    0    1    0    A3   A2   A1   A0   Right Channel, -1dB/step
+  0    0    1    1    0    B2   B1   B0   Right Channel, -10dB/step
+  0    1    1    1    1    0    0    M    2-Channel MUTE (M=1 -> MUTE=ON / M=0 -> MUTE=OFF)
+
+  ATTENUATION UNIT BIT
+  A3   AB2  AB1  AB0  ATT(-1) ATT(-10)
+  0    0    0    0     0      0
+  0    0    0    1    -1    -10
+  0    0    1    0    -2    -20
+  0    0    1    1    -3    -30
+  0    1    0    0    -4    -40
+  0    1    0    1    -5    -50
+  0    1    1    0    -6    -60
+  0    1    1    1    -7    -70
+  1    0    0    0    -8
+  1    0    0    1    -9
+
+*/
+
+#define PT2257_ADDR 0x88        //Chip address
+#define EVC_OFF     0b11111111  //Function OFF (-79dB)
+#define EVC_2CH_1   0b11010000  //2-Channel, -1dB/step
+#define EVC_2CH_10  0b11100000  //2-Channel, -10dB/step
+#define EVC_L_1     0b10100000  //Left Channel, -1dB/step
+#define EVC_L_10    0b10110000  //Left Channel, -10dB/step
+#define EVC_R_1     0b00100000  //Right Channel, -1dB/step
+#define EVC_R_10    0b00110000  //Right Channel, -10dB/step
+#define EVC_MUTE    0b01111000  //2-Channel MUTE
+
+class AudioControlPT2257 {
+  private:
+    uint8_t volume = 0;
+    bool mute = false;
+	byte level(uint8_t);
+	void writeVolume(uint8_t) ;
+	void writeMute(bool);
+	void off();
+	
+  public:
+	AudioControlPT2257();
+    void increase();
+    void decrease();
+    void toggleMute();
+    uint8_t getVolume();
+    bool getMute();
+    void serialPrintStateJSON();
+};
+#endif
